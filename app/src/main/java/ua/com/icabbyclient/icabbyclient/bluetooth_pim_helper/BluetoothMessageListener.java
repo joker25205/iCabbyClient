6b92f9ca4a6123fe85d9ab411951aba3c648bd12
Vasyl <vshcherban@ukeess.com>
2018-06-12 13:23:06 +0300

    Finished dispatch system

--- app/src/main/java/ua/com/icabbyclient/icabbyclient/bluetooth_pim_helper/BluetoothMessageListener.java
+++
@@ -1,5 +0,0 @@
-package ua.com.icabbyclient.icabbyclient.bluetooth_pim_helper;
-
-public interface BluetoothMessageListener {
-    void onBtRawMessage(int message, String rawMsg);
-}
--- app/src/main/java/ua/com/icabbyclient/icabbyclient/bluetooth_pim_helper/BluetoothMessageSender.java
+++
@@ -1,6 +0,0 @@
-package ua.com.icabbyclient.icabbyclient.bluetooth_pim_helper;
-
-public interface BluetoothMessageSender {
-
-    public void onBtSendRawMessage(int message, String rawMsg);
-}
